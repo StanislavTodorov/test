cd3edc86f3cd660a629fbed0e15312277a68d50e
stamboqt <stamboqt@yahoo.com>
2014-04-10 15:56:36 +0300

    test1

---
+++ register.php
@@ -0,0 +1,48 @@
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+<form action="reg_complete.php" method="post">
+  <table widght="100%" border="5" cellspacing="0">
+    <tr>
+      <td>
+	    Име:
+      </td>	
+      <td>
+	    <input type="text" name="ime">
+      </td>	  
+    </tr>
+	<tr>
+      <td>
+	    Фамилия:
+      </td>	
+      <td>
+	    <input type="text" name="familiq">
+      </td>	  
+    </tr>
+	<tr>
+      <td>
+	    Потребителско име:
+      </td>	
+      <td>
+	    <input type="text" name="username">
+      </td>	  
+    </tr>
+	<tr>
+      <td>
+	    Парола:
+      </td>	
+      <td>
+	    <input type="password" name="parola">
+      </td>	  
+    </tr>
+	<tr>
+      <td>
+      </td>	
+      <td>
+	    <input type="submit" name="Reg">
+      </td>	  
+    </tr>
+  </table>
+</form>
+</body>
